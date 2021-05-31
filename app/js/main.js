f2ca05442cc036112a652dda2cd77a5f710cde30
AnnaZubenko79 <annazubenko79@gmail.com>
2021-05-31 15:41:48 +0300

    added task weekproducts, news, new-design

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,26 @@ $(function(){
             duration: 0.2  
         }          
         });
+
+        var mixer = mixitup ('.new-design__content', {
+         animation: {
+             duration: 0.2  
+         }          
+         });
+
+        $('.dropdown--home').on('click', function(){
+            $('.dropdown__btn--home').toggleClass('dropdown__btn--home--active'); 
+            
+         });
+
+        $('.dropdown--pages').on('click', function(){
+            $('.dropdown__btn--pages').toggleClass('dropdown__btn--pages--active'); 
+            
+         });
+         $('.dropdown--home').on('click', function(){
+            $('.dropdown--home').toggleClass('dropdown--home--active');             
+         });
+         $('.dropdown--pages').on('click', function(){
+            $('.dropdown--pages').toggleClass('dropdown--pages--active');             
+         });
 });
